397a7215597b4dbd5f1086e4d5528ea0f9b84e1e
Colin Bender <cyoshiob@gmail.com>
2026-02-28 17:47:48 -0800

    Fully fleshed out first example script

--- Takeoff_Land.py
+++ Takeoff_Land.py
@@ -1,2 +1,44 @@
 import asyncio
 import logging
+
+from mavsdk import System
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def run():
+    drone = System()
+    await drone.connect(system_address="udpin://0.0.0.0:14540")
+
+    status_text_task = asyncio.ensure_future(print_status_text(drone))
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("-- Connected to drone!")
+            break
+
+    print("--Arming")
+    await drone.action.arm()
+
+    print("--Taking off")
+    await drone.action.takeoff()
+
+    await asyncio.sleep(10)
+
+    print("--Landing")
+    await drone.action.land()
+
+    status_text_task.cancel()
+
+
+async def print_status_text(drone):
+    try:
+        async for status_text in drone.telemetry.status_text():
+            print(f"Status: {status_text.type}: {status_text.text}")
+    except asyncio.CancelledError:
+        return
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
